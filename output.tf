9cbf09ace32e01344cd1054baa981c917d062feb
nigorasultonov <152802393+nigorasultonov@users.noreply.github.com>
2024-03-09 10:25:55 -0500

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,7 @@
+/*
+output "terraform_instance_public_ip" {
+  description = "this is the public ip of my terraform server instance"
+  value       = aws_instance.terraform_server.public_ip
+  sensitive   = true
+}
+*/
